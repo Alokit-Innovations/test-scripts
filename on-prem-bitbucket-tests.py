51928abdddb8f9a94fd1562ac3aff8c48eddd694
Muskan Paliwal <muskan10112002@gmail.com>
2023-09-29 11:57:32 +0530

    added error handling

--- on-prem-bitbucket-tests.py
+++ on-prem-bitbucket-tests.py
@@ -26,100 +26,132 @@ workspace = 'alokit-innovations-test'
 repo_name = 'on-prem-bitbucket-test-repo'
 webhook_url = 'https://968d-171-76-83-56.ngrok-free.app/api/bitbucket/callbacks/webhook'
 
-# Get the OAuth token
 def get_oauth_token(client_id, client_secret):
-    url = "https://bitbucket.org/site/oauth2/access_token"
-    response = requests.post(url, auth=(client_id, client_secret), data={'grant_type': 'client_credentials'})
-    response.raise_for_status()
-    return response.json()
+    try:
+        url = "https://bitbucket.org/site/oauth2/access_token"
+        response = requests.post(url, auth=(client_id, client_secret), data={'grant_type': 'client_credentials'})
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f'Error getting OAuth token: {e}')
+        raise
 
 def create_repo(workspace, repo_name, token):
-    url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}"
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.post(url, headers=headers, json={"scm": "git"})
-    response.raise_for_status()
-    return response.json()
+    try:
+        url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}"
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.post(url, headers=headers, json={"scm": "git"})
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f'Error creating repository: {e}')
+        raise
 
 def create_branch(workspace, repo_name, token, source_branch, destination_branch):
-    url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}/refs/branches"
-    headers = {"Authorization": f"Bearer {token}"}
-    data = {"name": source_branch, "target": {"hash": destination_branch}}
-    response = requests.post(url, headers=headers, json=data)
-    response.raise_for_status()
+    try:
+        url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}/refs/branches"
+        headers = {"Authorization": f"Bearer {token}"}
+        data = {"name": source_branch, "target": {"hash": destination_branch}}
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f'Error creating branch: {e}')
+        raise
 
 def add_and_commit_change(workspace, repo_name, token, branch, filename, content):
-    url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}/src"
-    headers = {"Authorization": f"Bearer {token}"}
-    data = {"message": "Add/Update file", "branch": branch}
-    files = {filename: (filename, content)}
-    response = requests.post(url, headers=headers, data=data, files=files)
-    response.raise_for_status()
-    return response.json()
+    try:
+        url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}/src"
+        headers = {"Authorization": f"Bearer {token}"}
+        data = {"message": "Add/Update file", "branch": branch}
+        files = {filename: (filename, content)}
+        response = requests.post(url, headers=headers, data=data, files=files)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f'Error adding and committing change: {e}')
+        raise
 
 def raise_pr(workspace, repo_name, token):
-    source_branch = "feature/dummy"
-    destination_branch = "main"
-    
-    create_branch(workspace, repo_name, token, source_branch, destination_branch)
-    
-    filename = "dummy_file.txt"
-    content = 'print("This is a modified dummy file")'
-    add_and_commit_change(workspace, repo_name, token, source_branch, filename, content)
-    
-    url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}/pullrequests"
-    headers = {"Authorization": f"Bearer {token}"}
-    data = {
-        "title": "Dummy PR",
-        "source": {"branch": {"name": source_branch}},
-        "destination": {"branch": {"name": destination_branch}},
-        "close_source_branch": True,
-        "reason": "Merging modified dummy feature",
-    }
-    response = requests.post(url, headers=headers, json=data)
-    response.raise_for_status()
-    return response.json()
+    try:
+        source_branch = "feature/dummy"
+        destination_branch = "main"
+        create_branch(workspace, repo_name, token, source_branch, destination_branch)
+        filename = "dummy_file.txt"
+        content = 'print("This is a modified dummy file")'
+        add_and_commit_change(workspace, repo_name, token, source_branch, filename, content)
+        url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}/pullrequests"
+        headers = {"Authorization": f"Bearer {token}"}
+        data = {
+            "title": "Dummy PR",
+            "source": {"branch": {"name": source_branch}},
+            "destination": {"branch": {"name": destination_branch}},
+            "close_source_branch": True,
+            "reason": "Merging modified dummy feature",
+        }
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f'Error raising PR: {e}')
+        raise
 
-def store_repo_data(name, workspace, auth_info, provider,metadata, git_url):
-    conn = psycopg2.connect(f"dbname={db_name} user={db_user} host={db_host} password={db_password} port={db_port}")
-    cur = conn.cursor()
-    query = """
-        INSERT INTO repos (repo_name, repo_owner, repo_provider, auth_info, metadata, git_url) 
-        VALUES (%s, %s, %s, %s, %s, %s)
-        ON CONFLICT (repo_name, repo_owner, repo_provider) DO UPDATE SET 
-        auth_info = EXCLUDED.auth_info, 
-        metadata = EXCLUDED.metadata,
-        git_url = EXCLUDED.git_url
-    """
-    params = (name, workspace, provider, auth_info, metadata, git_url)
-    cur.execute(query, params)
-    conn.commit()
-    cur.close()
-    conn.close()
+def store_repo_data(name, workspace, auth_info, provider, metadata, git_url):
+    try:
+        conn = psycopg2.connect(f"dbname={db_name} user={db_user} host={db_host} password={db_password} port={db_port}")
+        cur = conn.cursor()
+        query = """
+            INSERT INTO repos (repo_name, repo_owner, repo_provider, auth_info, metadata, git_url) 
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON CONFLICT (repo_name, repo_owner, repo_provider) DO UPDATE SET 
+            auth_info = EXCLUDED.auth_info, 
+            metadata = EXCLUDED.metadata,
+            git_url = EXCLUDED.git_url
+        """
+        params = (name, workspace, provider, auth_info, metadata, git_url)
+        cur.execute(query, params)
+        conn.commit()
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f'Error storing repo data: {e}')
+        raise
 
 def simulate_webhook_event(webhook_url, pr_info, repo_data):
-    payload = {
-        "pullrequest": pr_info,
-        "repository": repo_data
-    }
-    response = requests.post(webhook_url, json=payload)
-    response.raise_for_status()
+    try:
+        payload = {
+            "pullrequest": pr_info,
+            "repository": repo_data
+        }
+        response = requests.post(webhook_url, json=payload)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f'Error simulating webhook event: {e}')
+        raise
 
 def check_db_for_hunk_info(pr_number, repo_name, repo_owner, provider):
-    time.sleep(180)
-    conn = psycopg2.connect(f"dbname={db_name} user={db_user} host={db_host} password={db_password} port={db_port}")
-    cur = conn.cursor()
-    cur.execute("SELECT hunk_info FROM hunks WHERE review_id=%s and repo_name=%s and repo_owner=%s and repo_provider=%s", (pr_number, repo_name, repo_owner, provider))
-    row = cur.fetchone()
-    cur.close()
-    conn.close()
-    return bool(row)
+    try:
+        time.sleep(180)
+        conn = psycopg2.connect(f"dbname={db_name} user={db_user} host={db_host} password={db_password} port={db_port}")
+        cur = conn.cursor()
+        cur.execute("SELECT hunk_info FROM hunks WHERE review_id=%s and repo_name=%s and repo_owner=%s and repo_provider=%s", (pr_number, repo_name, repo_owner, provider))
+        row = cur.fetchone()
+        cur.close()
+        conn.close()
+        return bool(row)
+    except Exception as e:
+        print(f'Error checking DB for hunk info: {e}')
+        raise
 
 def delete_repo(workspace, repo_name, token):
-    url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}"
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.delete(url, headers=headers)
-    response.raise_for_status()
-
+    try:
+        url = f"https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_name}"
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.delete(url, headers=headers)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f'Error deleting repository: {e}')
+        raise
+    
 def main():
     auth_info = get_oauth_token(oauth_consumer_key, oauth_consumer_secret)
     expires_at = datetime.datetime.utcnow() + datetime.timedelta(seconds=auth_info["expires_in"])
